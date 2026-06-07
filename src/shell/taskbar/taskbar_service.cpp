cc2bacd96e514242e38030fe600adfd4fd2e71f8
Antal K <antalk2@gmail.com>
2026-06-07 18:30:37 +0200

    fmt

--- src/shell/taskbar/taskbar_service.cpp
+++ src/shell/taskbar/taskbar_service.cpp
@@ -2,20 +2,21 @@
 
 namespace seb::shell::taskbar {
 
-TaskbarModel TaskbarService::buildModel(const seb::SebSettings &settings, bool hasBattery)
+TaskbarModel TaskbarService::buildModel( const seb::SebSettings& settings
+                                       , bool hasBattery )
 {
     TaskbarModel model;
-    model.visible = settings.taskbar.enableTaskbar;
+    model.visible             = settings.taskbar.enableTaskbar;
     model.showApplicationInfo = settings.taskbar.showApplicationInfo;
-    model.showApplicationLog = settings.taskbar.showApplicationLog;
-    model.showProctoring = settings.taskbar.showProctoringNotification;
-    model.showVerificator = settings.taskbar.showVerificator;
-    model.showPower = hasBattery;
-    model.showNetwork = settings.taskbar.showNetwork;
-    model.showAudio = settings.taskbar.showAudio;
-    model.showKeyboardLayout = settings.taskbar.showKeyboardLayout;
-    model.showClock = settings.taskbar.showClock;
-    model.showQuit = settings.security.allowTermination;
+    model.showApplicationLog  = settings.taskbar.showApplicationLog;
+    model.showProctoring      = settings.taskbar.showProctoringNotification;
+    model.showVerificator     = settings.taskbar.showVerificator;
+    model.showPower           = hasBattery;
+    model.showNetwork         = settings.taskbar.showNetwork;
+    model.showAudio           = settings.taskbar.showAudio;
+    model.showKeyboardLayout  = settings.taskbar.showKeyboardLayout;
+    model.showClock           = settings.taskbar.showClock;
+    model.showQuit            = settings.security.allowTermination;
     return model;
 }
 
