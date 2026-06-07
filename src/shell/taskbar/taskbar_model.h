cbb3187566983ca3464f9b3917670ce279ae57ef
Antal K <antalk2@gmail.com>
2026-06-07 18:25:37 +0200

    fmt

--- src/shell/taskbar/taskbar_model.h
+++ src/shell/taskbar/taskbar_model.h
@@ -4,19 +4,18 @@
 
 namespace seb::shell::taskbar {
 
-struct TaskbarModel
-{
-    bool visible = true;
-    bool showApplicationInfo = false;
-    bool showApplicationLog = false;
-    bool showProctoring = false;
-    bool showVerificator = false;
-    bool showPower = false;
-    bool showNetwork = false;
-    bool showAudio = false;
-    bool showKeyboardLayout = false;
-    bool showClock = false;
-    bool showQuit = true;
+struct TaskbarModel {
+  bool visible             = true;
+  bool showApplicationInfo = false;
+  bool showApplicationLog  = false;
+  bool showProctoring      = false;
+  bool showVerificator     = false;
+  bool showPower           = false;
+  bool showNetwork         = false;
+  bool showAudio           = false;
+  bool showKeyboardLayout  = false;
+  bool showClock           = false;
+  bool showQuit            = true;
 };
 
 }  // namespace seb::shell::taskbar
