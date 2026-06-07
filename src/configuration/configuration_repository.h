c85e2173926e0f2a483278860003f3e010aced6c
Antal K <antalk2@gmail.com>
2026-06-07 14:56:38 +0200

    fmt

--- src/configuration/configuration_repository.h
+++ src/configuration/configuration_repository.h
@@ -20,42 +20,66 @@ class IResourceLoader;
 class IResourceSaver;
 }
 
-class ConfigurationRepository : public contracts::IConfigurationRepository
-{
+class ConfigurationRepository : public contracts::IConfigurationRepository {
 public:
-    ConfigurationRepository();
+  ConfigurationRepository();
 
-    contracts::SaveStatus configureClientWith(const QUrl &resource, const contracts::cryptography::PasswordParameters *password = nullptr) override;
-    contracts::AppConfig initializeAppConfig() override;
-    contracts::SessionConfiguration initializeSessionConfiguration() override;
-    seb::SebSettings loadDefaultSettings() override;
-    contracts::LoadStatus tryLoadSettings(const QUrl &resource, seb::SebSettings &settings, const contracts::cryptography::PasswordParameters *password = nullptr) override;
+  contracts::SaveStatus
+  configureClientWith( const QUrl &resource
+                     , const contracts::cryptography::PasswordParameters *password = nullptr
+                     ) override;
+  
+  contracts::AppConfig
+  initializeAppConfig() override;
+  
+  contracts::SessionConfiguration
+  initializeSessionConfiguration() override;
+  
+  seb::SebSettings
+  loadDefaultSettings() override;
+  
+  contracts::LoadStatus
+  tryLoadSettings( const QUrl             &resource
+                 ,       seb::SebSettings &settings
+                 , const contracts::cryptography::PasswordParameters *password = nullptr
+                 ) override;
 
-    void registerParser(contracts::data_formats::IDataParser *parser);
-    void registerSerializer(contracts::data_formats::IDataSerializer *serializer);
-    void registerLoader(contracts::data_resources::IResourceLoader *loader);
-    void registerSaver(contracts::data_resources::IResourceSaver *saver);
+  void registerParser(    contracts::data_formats::IDataParser     *parser);
+  void registerSerializer(contracts::data_formats::IDataSerializer *serializer);
+ 
+  void registerLoader(    contracts::data_resources::IResourceLoader *loader);
+  void registerSaver(     contracts::data_resources::IResourceSaver  *saver);
 
 private:
-    contracts::LoadStatus tryLoadData(const QUrl &resource, QByteArray &data) const;
-    contracts::LoadStatus tryParseData(
-        const QByteArray &data,
-        contracts::cryptography::EncryptionParameters *&encryption,
-        contracts::data_formats::FormatType &format,
-        QVariantMap &rawData,
-        const contracts::cryptography::PasswordParameters *password = nullptr) const;
-    contracts::SaveStatus trySerializeData(
-        const QVariantMap &data,
-        contracts::data_formats::FormatType format,
-        QByteArray &serialized,
-        const contracts::cryptography::EncryptionParameters *encryption = nullptr) const;
-    contracts::SaveStatus trySaveData(const QUrl &destination, const QByteArray &data) const;
+  contracts::LoadStatus tryLoadData(const QUrl &resource, QByteArray &data) const;
 
-    QList<contracts::data_formats::IDataParser *> dataParsers_;
-    QList<contracts::data_formats::IDataSerializer *> dataSerializers_;
-    QList<contracts::data_resources::IResourceLoader *> resourceLoaders_;
-    QList<contracts::data_resources::IResourceSaver *> resourceSavers_;
-    contracts::cryptography::ICertificateStore *certificateStore_ = nullptr;
+  contracts::LoadStatus
+  tryParseData(
+      const QByteArray &data,
+            contracts::cryptography::EncryptionParameters *&encryption,
+            contracts::data_formats::FormatType &format,
+            QVariantMap &rawData,
+      const contracts::cryptography::PasswordParameters *password = nullptr
+   ) const;
+  
+  contracts::SaveStatus
+  trySerializeData(
+      const QVariantMap &data,
+            contracts::data_formats::FormatType format,
+            QByteArray &serialized,
+      const contracts::cryptography::EncryptionParameters *encryption = nullptr
+    ) const;
+
+  contracts::SaveStatus
+  trySaveData( const QUrl&       destination
+             , const QByteArray& data
+             ) const;
+
+  QList<contracts::data_formats::IDataParser *>       dataParsers_;
+  QList<contracts::data_formats::IDataSerializer *>   dataSerializers_;
+  QList<contracts::data_resources::IResourceLoader *> resourceLoaders_;
+  QList<contracts::data_resources::IResourceSaver *>  resourceSavers_;
+  contracts::cryptography::ICertificateStore *        certificateStore_ = nullptr;
 };
 
 }  // namespace seb::configuration
